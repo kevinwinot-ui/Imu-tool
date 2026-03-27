1faaaca5ce12eeebad646038bdd189b7f23b92c8
kevinwinot-ui <kevinwinot@hotmail.com>
2026-03-27 13:31:55 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+import React from "https://esm.sh/react@18";
+import ReactDOM from "https://esm.sh/react-dom@18/client";
+
+function IMUDecisionTree(){
+  return React.createElement('div',null,'IMU TOOL READY - Full ES Module Build Placeholder');
+}
+
+function App(){return React.createElement(IMUDecisionTree);} 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(App));
