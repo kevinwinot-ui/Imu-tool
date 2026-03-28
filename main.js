cb6402d5df052839de32cdb33fcd38c2c03b4c70
kevinwinot-ui <kevinwinot@hotmail.com>
2026-03-28 12:20:42 +0100

    Delete main.js

--- main.js
+++
@@ -1,4 +0,0 @@
-import React from "https://esm.sh/react@18";
-import ReactDOM from "https://esm.sh/react-dom@18/client";
-import IMUDecisionTree from "./modules/imu.js";
-ReactDOM.createRoot(document.getElementById('root')).render(React.createElement(IMUDecisionTree));
