e16549a503556f211c1bdac7230011ef0ba273d9
kevinwinot-ui <kevinwinot@hotmail.com>
2026-03-28 12:20:28 +0100

    Delete imu.js

--- imu.js
+++
@@ -1,7 +0,0 @@
-export default function IMUDecisionTree(){return React.createElement('div',null,'IMU FULL MODULE BUILD PLACEHOLDER');}
-import Dashboard from "./ui/Dashboard.js";
-import ProfileSelect from "./ui/ProfileSelect.js";
-import Turn8 from "./ui/Turn8.js";
-import Turn9 from "./ui/Turn9.js";
-case "turn8": return React.createElement(Turn8);
-case "turn9": return React.createElement(Turn9);
